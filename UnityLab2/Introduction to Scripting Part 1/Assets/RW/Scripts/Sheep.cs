fe446abba2edb13da41593aaff461d665ecc0a7a
rgarciat <46629481+rgarciat@users.noreply.github.com>
2020-04-21 11:24:34 +0200

    lab2

---
+++ UnityLab2/Introduction to Scripting Part 1/Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Sheep : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float dropDestroyDelay; // 1
+    private Collider myCollider; // 2
+    private Rigidbody myRigidbody; // 3
+    public float runSpeed;
+    public float gotHayDestroyDelay;
+    private bool hitByHay;
+    private SheepSpawner sheepSpawner;
+
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+
+    }
+
+    public void SetSpawner(SheepSpawner spawner)
+    {
+        sheepSpawner = spawner;
+    }
+    private void HitByHay()
+    {
+        hitByHay = true; // 1
+        runSpeed = 0; // 2
+        Destroy(gameObject, gotHayDestroyDelay); // 3
+        sheepSpawner.RemoveSheepFromList(gameObject);
+
+    }
+    private void OnTriggerEnter(Collider other) // 1
+    {
+        if (other.CompareTag("Hay") && !hitByHay) // 2
+        {
+            Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+    }
+    private void Drop()
+    {
+        myRigidbody.isKinematic = false; // 1
+        myCollider.isTrigger = false; // 2
+        Destroy(gameObject, dropDestroyDelay); // 3
+    }
+}
